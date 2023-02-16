a088232d71d6f9756f34a6f6857acb3ab70efd67
JAPHETH <japho0740@gmail.com>
2023-02-16 14:11:04 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,11 @@
  *
  * Return 1 (success)
  */
-int main(void)
+int main(void)/**
+ * int main - A c program that prints a line to the standard error
+ *
+ * Return 1 (success)
+ */
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
