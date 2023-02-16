d2fe8adf0ede627ffff8c05d706dd78a17a88e9d
JAPHETH <japho0740@gmail.com>
2023-02-16 14:33:32 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
+ * main - Entry point
  *
- * Return 1 (success)
- * If the program executes properly it returns 1
+ * Description: 'A c program that prints a line to the standard error'
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
