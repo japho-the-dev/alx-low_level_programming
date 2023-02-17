bd870639e1357aa42a5b3b2bef9e0ea94b97dc56
JAPHETH <japho0740@gmail.com>
2023-02-17 12:41:24 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int i;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
@@ -18,7 +19,7 @@ int main(void)
 			{
 				putchar(i + '0');
 				putchar(j + '0');
-				if (i+j != 17)
+				if (i + j != 17)
 				{
 					putchar(',');
 					putchar(' ');
