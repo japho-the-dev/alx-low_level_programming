fd38760e99221fc3d2efacef7e5a6c1b5ab4f65f
JAPHETH <japho0740@gmail.com>
2023-02-21 11:38:52 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,9 @@
 #include "main.h"
 
 /**
- * main - print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- * followed by a new line
- * Return: always void (Success)
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  */
-/**
- * Print_alphabet_x10 - main function that print_alphabet 10 times
- * Character - @arg1 : Accept character data types
- * int - @arg2: accepts integers (10 timws)
- * Return: Always 0 (Success)
- */
+
 void print_alphabet_x10(void)
 {
 	char ch;
