590a805c12ce657d9c6b757fee2d551f14f9623d
JAPHETH <japho0740@gmail.com>
2023-03-14 12:25:34 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -28,14 +28,13 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-	for (n = 0; av[i][n]; n++)
+		for (n = 0; av[i][n]; n++)
 		str[r] = av[i][n];
 		r++;
 	}
 	if (str[r] == '\0')
 	{
 		str[r++] = '\n';
-	}
 	}
 	return (str);
 }
