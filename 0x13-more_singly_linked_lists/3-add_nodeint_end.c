28d10947b5842d6134cc2b3a4abf838914b73efb
JAPHETH <japho0740@gmail.com>
2023-04-05 14:53:46 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *cursor = *head;
 
 	n_node = malloc(sizeof(listint_t));
-	if (new_node != NULL)
+	if (n_node != NULL)
 	{
 		n_node->n = n;
 		n_node->next = NULL;
