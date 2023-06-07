992d32dbbcb4f85b2405f012126e07f60d05ba67
JAPHETH <japho0740@gmail.com>
2023-06-07 13:39:01 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,16 +8,16 @@
  */
 
 int _pow_recursion(int x, int y) {
-    // Base case: y equals 0, return 1
+    /* Base case: y equals 0, return 1 */
     if (y == 0) {
         return 1;
     }
 
-    // If y is negative, return -1
+    /* If y is negative, return -1 */
     if (y < 0) {
         return -1;
     }
 
-    // Recursive case: multiply x with x raised to y - 1
+    /* Recursive case: multiply x with x raised to y - 1 */
     return x * _pow_recursion(x, y - 1);
 }
