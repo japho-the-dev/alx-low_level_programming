54ce7d64c8b5e08b7cf9163decb1b397e2260e79
JAPHETH <japho0740@gmail.com>
2023-06-09 13:24:26 -0700

    Solutions

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,10 +9,9 @@
  * Return: If the program receives two arguments - 0.
  *         If the program does not receive two arguments - 1.
  */
-
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2, prod;
 
 	if (argc != 3)
 	{
@@ -20,11 +19,11 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	result = num1 * num2;
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	prod = num1 * num2;
 
-	printf("%d\n", result);
+	printf("%d\n", prod);
 
 	return (0);
 }
