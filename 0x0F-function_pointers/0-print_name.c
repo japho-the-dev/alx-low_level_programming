465ecbf76f4f80c95034cddfd2f0e20e9ac3b4d3
JAPHETH <japho0740@gmail.com>
2023-06-21 12:58:30 -0700

    Soltions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "function_pointers.h"
 
@@ -8,7 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
